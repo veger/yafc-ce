24c716af80f60184ddffd5b63d2e2a7d3521338b
Maarten Bezemer <maarten.bezemer@gmail.com>
2023-01-30 15:17:19 +0100

    Compress builds

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@
 dotnet publish YAFC/YAFC.csproj -r win-x64 -c Release -o Build/Windows -p:PublishTrimmed=true
 dotnet publish YAFC/YAFC.csproj -r osx-x64 --self-contained false -c Release -o Build/OSX
 dotnet publish YAFC/YAFC.csproj -r linux-x64 --self-contained false -c Release -o Build/Linux
+
+pushd Build
+zip -r Linux.zip Linux
+zip -r OSX.zip OSX
+zip -r Windows.zip Windows
+popd
