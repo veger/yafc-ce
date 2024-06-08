d35bbfceb766eec57cf2ab47de89954bafc0aead
Dale McCoy <21223975+DaleStan@users.noreply.github.com>
2024-06-08 17:51:15 -0400

    feat: Allow clicking on a linked recipe to inspect that recipe's links.

--- Yafc/Workspace/ProductionTable/ProductionLinkSummaryScreen.cs
+++ Yafc/Workspace/ProductionTable/ProductionLinkSummaryScreen.cs
@@ -1,29 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using SDL2;
 using Yafc.Model;
 using Yafc.UI;
 
 namespace Yafc;
 
 public class ProductionLinkSummaryScreen : PseudoScreen, IComparer<(RecipeRow row, float flow)> {
-    private readonly ProductionLink link;
-    private readonly List<(RecipeRow row, float flow)> input = [];
-    private readonly List<(RecipeRow row, float flow)> output = [];
+    private readonly Stack<ProductionLink> links = new();
+
+    private ProductionLink link;
+    private List<(RecipeRow row, float flow)> input;
+    private List<(RecipeRow row, float flow)> output;
     private float totalInput, totalOutput;
     private readonly ScrollArea scrollArea;
 
     private ProductionLinkSummaryScreen(ProductionLink link) {
         scrollArea = new ScrollArea(30, BuildScrollArea);
-        this.link = link;
         CalculateFlow(link);
     }
 
     private void BuildScrollArea(ImGui gui) {
         gui.BuildText("Production: " + DataUtils.FormatAmount(totalInput, link.goods.flowUnitOfMeasure), Font.subheader);
-        BuildFlow(gui, input, totalInput);
+        BuildFlow(gui, input, totalInput, false);
         gui.spacing = 0.5f;
         gui.BuildText("Consumption: " + DataUtils.FormatAmount(totalOutput, link.goods.flowUnitOfMeasure), Font.subheader);
-        BuildFlow(gui, output, totalOutput);
+        BuildFlow(gui, output, totalOutput, true);
         if (link.amount != 0) {
             gui.spacing = 0.5f;
             gui.BuildText((link.amount > 0 ? "Requested production: " : "Requested consumption: ") + DataUtils.FormatAmount(MathF.Abs(link.amount),
@@ -47,16 +51,57 @@ public class ProductionLinkSummaryScreen : PseudoScreen, IComparer<(RecipeRow ro
 
     protected override void ReturnPressed() => Close();
 
-    private void BuildFlow(ImGui gui, List<(RecipeRow row, float flow)> list, float total) {
+    private void BuildFlow(ImGui gui, List<(RecipeRow row, float flow)> list, float total, bool isLinkOutput) {
         gui.spacing = 0f;
         foreach (var (row, flow) in list) {
             string amount = DataUtils.FormatAmount(flow, link.goods.flowUnitOfMeasure);
-            _ = gui.BuildFactorioObjectButtonWithText(row.recipe, amount, tooltipOptions: DrawParentRecipes(row.owner, "recipe"));
+            if (gui.BuildFactorioObjectButtonWithText(row.recipe, amount, tooltipOptions: DrawParentRecipes(row.owner, "recipe")) == Click.Left) {
+                // Find the corresponding links associated with the clicked recipe,
+                IEnumerable<Goods> goods = isLinkOutput ? row.recipe.products.Select(p => p.goods) : row.recipe.ingredients.Select(i => i.goods);
+                Dictionary<Goods, ProductionLink> links = goods
+                    .Select(goods => { row.FindLink(goods, out ProductionLink? link); return (goods, link: link!); })
+                    .Where(x => x.link != null)
+                    .ToDictionary(x => x.goods, x => x.link);
+                links.Remove(link.goods); // except the current one, only applicable for recipes like kovarex that have catalysts.
+
+                // Jump to the only link, or offer a selection of links to inspect next.
+                if (links.Count == 1) {
+                    changeLinkView(links.First().Value);
+                }
+                else {
+                    gui.ShowDropDown(drawLinks);
+                }
+
+                void drawLinks(ImGui gui) {
+                    if (links.Count == 0) {
+                        string text = isLinkOutput ? "This recipe has no linked products." : "This recipe has no linked ingredients";
+                        gui.BuildText(text, TextBlockDisplayStyle.ErrorText);
+                    }
+                    else {
+                        IComparer<Goods> comparer = DataUtils.DefaultOrdering;
+                        if (isLinkOutput && row.recipe.mainProduct is Goods mainProduct) {
+                            comparer = DataUtils.CustomFirstItemComparer(mainProduct, comparer);
+                        }
+
+                        string header = isLinkOutput ? "Select product link to inspect" : "Select ingredient link to inspect";
+                        ObjectSelectOptions<Goods> options = new(header, comparer, int.MaxValue);
+                        if (gui.BuildInlineObjectList(links.Keys, out Goods? selected, options) && gui.CloseDropdown()) {
+                            changeLinkView(links[selected]);
+                        }
+                    }
+                }
+            }
+
             if (gui.isBuilding) {
                 var lastRect = gui.lastRect;
                 lastRect.Width *= (flow / total);
                 gui.DrawRectangle(lastRect, SchemeColor.Primary);
             }
+        }
+
+        void changeLinkView(ProductionLink newLink) {
+            links.Push(link);
+            CalculateFlow(newLink);
         }
     }
 
@@ -87,9 +132,19 @@ public class ProductionLinkSummaryScreen : PseudoScreen, IComparer<(RecipeRow ro
         }
     };
 
+    public override bool KeyDown(SDL.SDL_Keysym key) {
+        if (key.scancode is SDL.SDL_Scancode.SDL_SCANCODE_BACKSPACE or SDL.SDL_Scancode.SDL_SCANCODE_KP_BACKSPACE && links.Count > 0) {
+            CalculateFlow(links.Pop());
+            return true;
+        }
+        return base.KeyDown(key);
+    }
+
+    [MemberNotNull(nameof(link), nameof(input), nameof(output))]
     private void CalculateFlow(ProductionLink link) {
-        totalInput = 0;
-        totalOutput = 0;
+        // changeLinkView calls this while iterating over one of these lists. We must create new lists rather than editing the existing ones.
+        List<(RecipeRow row, float flow)> input = [], output = [];
+        totalInput = totalOutput = 0;
         foreach (var recipe in link.capturedRecipes) {
             float production = recipe.GetProductionForRow(link.goods);
             float consumption = recipe.GetConsumptionForRow(link.goods);
@@ -106,6 +161,11 @@ public class ProductionLinkSummaryScreen : PseudoScreen, IComparer<(RecipeRow ro
         }
         input.Sort(this);
         output.Sort(this);
+
+        this.input = input;
+        this.output = output;
+        this.link = link;
+
         Rebuild();
         scrollArea.RebuildContents();
     }
