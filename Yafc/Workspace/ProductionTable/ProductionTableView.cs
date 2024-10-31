6bc35a4d6684f115da55b5d787ce3fba10d7b2f4
Dale McCoy <21223975+DaleStan@users.noreply.github.com>
2024-10-30 22:41:14 -0400

    fix: Show the synthetic items as boxes on disabled recipes.

--- Yafc/Workspace/ProductionTable/ProductionTableView.cs
+++ Yafc/Workspace/ProductionTable/ProductionTableView.cs
@@ -538,7 +538,7 @@ goodsHaveNoProduction:;
                 }
                 if (recipe.fixedIngredient == Database.itemInput || recipe.Ingredients.Count() >= Project.current.preferences.minForTotalItems) {
                     grid.Next();
-                    view.BuildGoodsIcon(gui, Database.itemInput, null, recipe.Ingredients.Where(i => i.Goods is Item).Sum(i => i.Amount),
+                    view.BuildGoodsIcon(gui, recipe.hierarchyEnabled ? Database.itemInput : null, null, recipe.Ingredients.Where(i => i.Goods is Item).Sum(i => i.Amount),
                         ProductDropdownType.Ingredient, recipe, recipe.linkRoot, HintLocations.None);
                 }
             }
@@ -559,7 +559,7 @@ goodsHaveNoProduction:;
                 }
                 if (recipe.fixedProduct == Database.itemOutput || recipe.Products.Count() >= Project.current.preferences.minForTotalItems) {
                     grid.Next();
-                    view.BuildGoodsIcon(gui, Database.itemOutput, null, recipe.Products.Where(i => i.Goods is Item).Sum(i => i.Amount),
+                    view.BuildGoodsIcon(gui, recipe.hierarchyEnabled ? Database.itemOutput : null, null, recipe.Products.Where(i => i.Goods is Item).Sum(i => i.Amount),
                         ProductDropdownType.Product, recipe, recipe.linkRoot, HintLocations.None);
                 }
             }
