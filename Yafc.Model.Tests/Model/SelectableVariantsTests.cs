41f20bd5e6f1d87c56e056b4af8de3ffce84be3c
Dale McCoy <21223975+DaleStan@users.noreply.github.com>
2024-11-04 00:16:11 -0500

    chore: update nuget packages.

--- Yafc.Model.Tests/Model/SelectableVariantsTests.cs
+++ Yafc.Model.Tests/Model/SelectableVariantsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Yafc.Model.Tests.Model;
@@ -6,7 +7,7 @@ namespace Yafc.Model.Tests.Model;
 [Collection("LuaDependentTests")]
 public class SelectableVariantsTests {
     [Fact]
-    public void CanSelectVariantFuel_VariantFuelChanges() {
+    public async Task CanSelectVariantFuel_VariantFuelChanges() {
         Project project = LuaDependentTestHelper.GetProjectForLua();
 
         ProjectPage page = new ProjectPage(project, typeof(ProductionTable));
@@ -15,16 +16,16 @@ public class SelectableVariantsTests {
         RecipeRow row = table.GetAllRecipes().Single();
 
         // Solve is not necessary in this test, but I'm calling it in case we decide to hide the fuel on disabled recipes.
-        table.Solve((ProjectPage)table.owner).Wait();
+        await table.Solve((ProjectPage)table.owner);
         Assert.Equal("steam@165", row.FuelInformation.Goods.name);
 
         row.fuel = row.FuelInformation.Variants[1];
-        table.Solve((ProjectPage)table.owner).Wait();
+        await table.Solve((ProjectPage)table.owner);
         Assert.Equal("steam@500", row.FuelInformation.Goods.name);
     }
 
     [Fact]
-    public void CanSelectVariantFuelWithFavorites_VariantFuelChanges() {
+    public async Task CanSelectVariantFuelWithFavorites_VariantFuelChanges() {
         Project project = LuaDependentTestHelper.GetProjectForLua();
         project.preferences.ToggleFavorite(Database.fluids.all.Single(c => c.name == "steam@500"));
 
@@ -34,16 +35,16 @@ public class SelectableVariantsTests {
         RecipeRow row = table.GetAllRecipes().Single();
 
         // Solve is not necessary in this test, but I'm calling it in case we decide to hide the fuel on disabled recipes.
-        table.Solve((ProjectPage)table.owner).Wait();
+        await table.Solve((ProjectPage)table.owner);
         Assert.Equal("steam@500", row.FuelInformation.Goods.name);
 
         row.fuel = row.FuelInformation.Variants[0];
-        table.Solve((ProjectPage)table.owner).Wait();
+        await table.Solve((ProjectPage)table.owner);
         Assert.Equal("steam@165", row.FuelInformation.Goods.name);
     }
 
     [Fact]
-    public void CanSelectVariantIngredient_VariantIngredientChanges() {
+    public async Task CanSelectVariantIngredient_VariantIngredientChanges() {
         Project project = LuaDependentTestHelper.GetProjectForLua();
 
         ProjectPage page = new ProjectPage(project, typeof(ProductionTable));
@@ -52,11 +53,11 @@ public class SelectableVariantsTests {
         RecipeRow row = table.GetAllRecipes().Single();
 
         // Solve is necessary here: Disabled recipes have null ingredients (and products), and Solve is the call that updates hierarchyEnabled.
-        table.Solve((ProjectPage)table.owner).Wait();
+        await table.Solve((ProjectPage)table.owner);
         Assert.Equal("steam@165", row.Ingredients.Single().Goods.name);
 
         row.ChangeVariant(row.Ingredients.Single().Goods, row.Ingredients.Single().Variants[1]);
-        table.Solve((ProjectPage)table.owner).Wait();
+        await table.Solve((ProjectPage)table.owner);
         Assert.Equal("steam@500", row.Ingredients.Single().Goods.name);
     }
 
