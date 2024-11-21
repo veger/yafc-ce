3750df6995ffe714f49f48041353744234ac60ac
shpaass <chill.gtnh@outlook.com>
2024-11-21 20:06:13 +0100

    feat: add a self-contained Windows deliverable

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ VERSION=$(grep -oPm1 "(?<=<AssemblyVersion>)[^<]+" Yafc/Yafc.csproj)
 echo "Building YAFC version $VERSION..."
 
 dotnet publish Yafc/Yafc.csproj -r win-x64 -c Release -o Build/Windows
+dotnet publish Yafc/Yafc.csproj -r win-x64 --self-contained -c Release -o Build/Windows-self-contained
 dotnet publish Yafc/Yafc.csproj -r osx-x64 --self-contained false -c Release -o Build/OSX
 dotnet publish Yafc/Yafc.csproj -r osx-arm64 --self-contained false -c Release -o Build/OSX-arm64
 dotnet publish Yafc/Yafc.csproj -r linux-x64 --self-contained false -c Release -o Build/Linux
@@ -21,5 +22,6 @@ tar czf Yafc-CE-Linux-$VERSION.tar.gz Linux
 tar czf Yafc-CE-OSX-intel-$VERSION.tar.gz OSX
 tar czf Yafc-CE-OSX-arm64-$VERSION.tar.gz OSX-arm64
 zip -r Yafc-CE-Windows-$VERSION.zip Windows
+zip -r Yafc-CE-Windows-self-contained-$VERSION.zip Windows-self-contained
 popd
 
