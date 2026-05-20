d2a0c315153c296f4420df618a5a495f13a8ca37
Dale McCoy <21223975+DaleStan@users.noreply.github.com>
2026-05-19 22:32:28 -0400

    re-fix(#626): Fix regression in e7985e2 that broke loading 1.1.

--- Yafc.Parser/Data/FactorioDataDeserializer.cs
+++ Yafc.Parser/Data/FactorioDataDeserializer.cs
@@ -819,7 +819,7 @@ nextWeightCalculation:;
 
     private Goods? LoadItemOrFluid(LuaTable table, bool useTemperature, bool isAlwaysItem) {
         string? type = null;
-        if (isAlwaysItem || table.Get("type", out type)) {
+        if ((factorioVersion >= v2_0 && isAlwaysItem) || table.Get("type", out type)) {
             if (table.Get("name", out string? name)) {
                 if (isAlwaysItem || type == "item") {
                     return GetObject<Item>(table);
